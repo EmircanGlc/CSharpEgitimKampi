3dc112e4214f01c7cf1c64ad55077955b5c39ee5
user <user@ALFA_3250_AIO>
2025-01-24 16:55:34 +0300

    8.ders methodlar bitti.

---
+++ 07_ForeachLoop/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Foreach Döngüsü
+
+            //Foreach(1;2;3;4)
+            //Değişken Türü
+            //Değişken Adı
+            //3:In
+            //4:Liste,Koleksiyon,Dizi
+
+            //Dizideki değerleri yazdırma
+
+            //string[] cities = { "milano", "roma", "budapeşte", "ankara", "istanbul", "varşova" };
+
+            //foreach (string x in cities) 
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33,41,205,6578,10394};
+            //foreach (int number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
+
+            //foreach (int number in numbers)  //Liste içerisinden bir number al ve 2 'ye tam bölünebiliyorsa ekrana yazdır.
+            //{
+            //    if (number%2==0)
+            //    {
+            //        Console.WriteLine(number);
+            //    }
+
+            //}
+
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
+            //int total = 0;
+
+            //foreach (int i in numbers)
+            //{
+            //    total += i;
+            //}
+            //Console.WriteLine(total);
+
+
+            //List<int> numbers = new List<int>()
+            //{
+            //    1,2,3,4,5,8
+            //};
+
+            //foreach (int number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //Bir kelimeyi harflerine ayıran örnek.
+
+            //string word = "Merhaba";
+
+            //foreach (char s in word)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+
+            #endregion
+
+            #region  Örnek Sınav Sistemi Uygulaması
+
+            Console.WriteLine("******C# Eğitim Kampı Sınav Uygulması********");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("-----------");
+
+            Console.Write("Sınıfınızda kaç öğrenci var:");
+            int studentCount = int.Parse(Console.ReadLine());
+            Console.WriteLine("------------");
+
+
+            //Öğrenci isimlerini ve not ortalamalarını saklayacak diziler
+            string[] studentNames = new string[studentCount]; //Öğrenci isimlerinin boyutunu öğrenci adetinden al.
+            double[] studentExamAvg = new double[studentCount];
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.Write($"{i+1}. öğrencinin ismini giriniz: ");
+                studentNames[i] = Console.ReadLine();
+
+                double totalExamResult = 0;
+
+                //Her öğrenci için 3 sınav notu girişi
+
+                for (int j = 0; j < 3; j++) 
+                {
+                    Console.Write($"{studentNames[i]} isimli öğrencinin {j+1}. sınav notunu giriniz: ");
+                    double value =double.Parse(Console.ReadLine());
+                    totalExamResult+= value; //notları topla.
+
+                }
+
+                studentExamAvg[i]= totalExamResult / 3; //studentexamAvg 'ın i'ninci değeri totalExamResult dan gelen değerin 3 e bölümüne eşittir.
+            }
+            //Öğrencilerin ort.sonuçları ve geçip kalma durumunu görelim.
+            for (int i = 0; i <studentCount; i++)
+            {
+                Console.WriteLine($"{studentNames[i]} adlı öğrencinin ortalaması: {studentExamAvg[i]}");
+
+                if (studentExamAvg[i] >= 50)
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersten geçti...");
+                }
+                else
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersten kaldı...");
+                }
+            }
+
+           
+
+
+            #endregion
+
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ 08_Methots/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methots
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Void Metotlar
+
+            //()
+            //Geriye değer döndürmeyen metotlar
+            //Customer--->Listele,Ekle,Sil,Güncelle
+            //Void ise geriye değer döndürmez.
+
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Ali Yıldız");
+            //    Console.WriteLine("Ayşe Yıldız");
+            //    Console.WriteLine("Hakan Öztürk");
+            //    Console.WriteLine("Merve Çınar");
+            //}
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+
+
+            //void Sum()
+            //{
+            //    int x = 1;
+            //    int y=2;
+            //    int z = x + y;
+            //    Console.WriteLine(z);
+            //}
+
+
+            //Sum();
+            //Console.Read();
+
+
+            #endregion
+
+            #region Geriye değer döndürmeyen string parametreli metotlar
+
+            //void WriteMethod(string customerName)
+            //{
+            //    Console.WriteLine(customerName);
+            //}
+
+            //WriteMethod("Mehmet Yıldırım");
+
+
+            //void CustomerCard(string name,string surName)
+            //{
+            //    Console.WriteLine("Müşteri : " + name + " " + surName);
+            //}
+
+
+            //CustomerCard("Mehmet", "Yıldız");
+            //CustomerCard("Ayşegül","Kaya");
+
+            #endregion
+
+
+            #region Geriye değer döndürmeyen int parametreli metotlar
+
+
+            //void Sum(int number1, int number2,int number3) 
+            //{
+
+            //    int result = number1 + number2 + number3;
+            //    Console.WriteLine(result);
+
+            //}
+
+            //Sum(4, 5, 6);
+
+
+
+
+            #endregion
+
+            #region Geriye Değer Döndüren Metotlar
+
+            //string CustomerName()
+            //{
+            //    return "Emircan Güleç";
+            //}
+
+            //CustomerName();
+
+            //string StudentCard()
+            //{
+            //    string name = "Ali";
+            //    string surname = "Kılıç";
+            //    return name + " " + surname;
+            //}
+            //Console.WriteLine(StudentCard());
+
+            #endregion
+
+            #region Geriye Değer Döndüren String  Parametreli Metotlar
+
+            string CountryCard(string countryName,string capital,string flagColor) 
+            {
+                string cardInfo ="Ülke: " + countryName + " - Başkent: " + capital + " - Bayrak Rengi:  " + flagColor;
+                return cardInfo;
+            }
+
+            string x, y, z;
+            Console.Write("Ülke Adını Giriniz: ");
+            x = Console.ReadLine();
+
+            Console.Write("Başkenti Giriniz: ");
+            y = Console.ReadLine();
+
+            Console.Write("Bayrak Rengini Giriniz: ");
+            z = Console.ReadLine();
+
+            if (x == "Türkiye")
+            {
+                Console.WriteLine("******************************");
+                Console.WriteLine("Şanlı ve Yüce Türk Bayrağına ve Şehitlerine Selam Olsun...");
+                Console.WriteLine("******************************");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            
+           
+            Console.WriteLine(CountryCard(x,y,z));
+            
+
+            
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
